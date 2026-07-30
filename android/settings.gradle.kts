e6cb36e13874722ac71d052e3f564ff2c07e47f1
Hassan Ali <hassanali7124295@gmail.com>
2026-07-30 14:36:15 +0500

    Fix settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,11 +1,6 @@
 pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
-    }
+    val flutterSdkPath = System.getenv("FLUTTER_ROOT")
+        ?: throw GradleException("FLUTTER_ROOT environment variable is not set")
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
